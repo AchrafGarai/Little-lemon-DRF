e4d0c3544bc1da377695fd6d486a9d30e1e3ecbc
AchrafGarai <achrafgarai@hotmail.com>
2023-03-20 21:16:48 +0100

    Added Post request

--- booking/views.py
+++ booking/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from booking.models import Booking, Menu
 from booking.serializers import BookingSerializer, MenuSerializer
 from rest_framework.decorators import permission_classes
-
+from rest_framework import status
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -14,16 +14,32 @@ def index(request):
 def hello_world(request):
     return Response({'message': 'Hello, world!'})
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def get_all_bookings(request):
-    bookings = Booking.objects.all()
-    serializer = BookingSerializer(bookings, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        bookings = Booking.objects.all()
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Invalid Format"} ,status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def get_all_menus(request):
-    menus = Menu.objects.all()
-    serializer = MenuSerializer(menus, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        menus = Menu.objects.all()
+        serializer = MenuSerializer(menus, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Invalid Format"} ,status.HTTP_404_NOT_FOUND)
